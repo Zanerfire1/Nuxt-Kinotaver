e520b637f2157918a31851825ef1fd481f020727
ИС-22-17Б <@POLYTECH2.KZ>
2024-04-19 12:51:59 +0500

    penis

---
+++ layouts/default.vue
@@ -0,0 +1,37 @@
+<template>
+        <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/public/fusion3.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+      KinoTower
+    </a>
+    <form class="d-fiex">
+        <button class="btn btn-outline-primary me-3">Sign Up</button>
+        <button class="btn btn-outline-success">Sign in</button>
+    </form>
+   </div>
+    </nav>
+    <div class="container py-2 mb-auto" style="min-height: calc(100vh-190px);">
+      <slot />
+    </div>
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <div class="col-md-4 d-flex align-items-center">
+      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Kinotower, Inc</span>
+    </div>
+    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+      <li class="ms-3"><a class="text-body-secondary" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
+    </ul>
+</footer>
+</template>
+
+<style>
+    .container {
+        min-height: calc(100vh - 40px - 150px);
+    }
+</style>
+    
---
+++ pages/index.vue
@@ -0,0 +1,164 @@
+<template>
+    <div class="row my-4">
+        <div class="col-md-4">
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Select Genre...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+        </div>
+        <div class="col-md-4">
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Select Country...</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+        </div>
+        <div class="col-md-3">
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Sort By</option>
+              <option value="name">Name</option>
+              <option value="year">Year</option>
+              <option value="rating">Rating</option>
+            </select>
+        </div>
+        <div class="col-md-1">
+           <button class="btn btn-outline-warning">Recet</button> 
+        </div>
+    </div>
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://kinojump.com/uploads/posts/2022-07/neverojatnoe-prikljuchenie-dzhodzho-zolotoj-veter.webp" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Невероятные приключения ДжоДжо: Золотой ветер</h5>
+        <p class="card-text">Рейтинг: 5</p>
+        <p class="card-text">Длительность: 39 серий, 25 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение, Комедия</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://ru-images.kinorium.com/movie/1080/2005077.jpg?1684410495" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Человке паук Паутина вселенных</h5>
+        <p class="card-text">Рейтинг: 4.5</p>
+        <p class="card-text">Длительность: 200 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://ru-images.kinorium.com/movie/1080/2005077.jpg?1684410495" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Человке паук Паутина вселенных</h5>
+        <p class="card-text">Рейтинг: 4.5</p>
+        <p class="card-text">Длительность: 200 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://ru-images.kinorium.com/movie/1080/2005077.jpg?1684410495" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Человке паук Паутина вселенных</h5>
+        <p class="card-text">Рейтинг: 4.5</p>
+        <p class="card-text">Длительность: 200 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://ru-images.kinorium.com/movie/1080/2005077.jpg?1684410495" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Человке паук Паутина вселенных</h5>
+        <p class="card-text">Рейтинг: 4.5</p>
+        <p class="card-text">Длительность: 200 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://ru-images.kinorium.com/movie/1080/2005077.jpg?1684410495" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Человке паук Паутина вселенных</h5>
+        <p class="card-text">Рейтинг: 4.5</p>
+        <p class="card-text">Длительность: 200 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://ru-images.kinorium.com/movie/1080/2005077.jpg?1684410495" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Человке паук Паутина вселенных</h5>
+        <p class="card-text">Рейтинг: 4.5</p>
+        <p class="card-text">Длительность: 200 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://ru-images.kinorium.com/movie/1080/2005077.jpg?1684410495" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Человке паук Паутина вселенных</h5>
+        <p class="card-text">Рейтинг: 4.5</p>
+        <p class="card-text">Длительность: 200 мин</p>
+        <p class="card-text">Экшен, Фантастика, Приключение</p>
+      </div>
+      <div class="card-footer text-end">
+       <button class="btn btn-outline-primary">Смотреть</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!--pagination list films-->
+<nav aria-label="Page navigation " class="mt-4 d-flex justify-content-center">
+  <ul class="pagination">
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+</template>
